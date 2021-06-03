aa39654ee591a335704267ed2a46bf44681d1498
hgnam2001 <85303199+hgnam2001@users.noreply.github.com>
2021-06-03 23:06:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu chị nhiều nhiều �😘',
     messDesc: 'Tối nay 7h anh qua đón nhé bé yêu <3.',
     btnAccept: 'Oki lun <3',
-    messLink: 'http://fb.com//m.me/namteo.1409 
+    messLink: 'http://fb.com//m.me/namteo.1409',
 }
